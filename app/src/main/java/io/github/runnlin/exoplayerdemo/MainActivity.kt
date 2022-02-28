57c7d1e33aab8543f97c6d71060161921a59df74
runnlin <z77072716565@gmail.com>
2022-02-28 09:56:40 +0800

    添加识别缺失的音视频文件后缀； 加入播放中识别（测试）；

--- app/src/main/java/io/github/runnlin/exoplayerdemo/MainActivity.kt
+++ app/src/main/java/io/github/runnlin/exoplayerdemo/MainActivity.kt
@@ -1,21 +1,21 @@
 package io.github.runnlin.exoplayerdemo
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.media.AudioManager
+import android.media.AudioPlaybackConfiguration
 import android.media.MediaMetadataRetriever
-import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.os.CountDownTimer
-import android.os.Environment
+import android.os.*
 import android.provider.Settings
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
@@ -37,14 +37,16 @@ import java.util.*
 
 
 private const val TAG = "ExoMainActivity"
-private var DELAY_TIME: Long = 3L
-private var rootPath = "/storage/self/primary/Movies"
+private var DELAY_TIME: Long = 20L
+@SuppressLint("SdCardPath")
+private var rootPath = "/sdcard/Movies"
 
 class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, Player.Listener {
 
     private lateinit var _recyclerView: RecyclerView
     private lateinit var _floatBtn: ExtendedFloatingActionButton
     private lateinit var _playerView: PlayerView
+    private lateinit var _isMusicActive: ImageView
     private lateinit var _player: ExoPlayer
     private lateinit var _binding: ActivityMainBinding
     private lateinit var mediaListAdapter: MediaListAdapter
@@ -54,6 +56,7 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
     private lateinit var _scanFile: ScanFileUtil
 
     private var isAutoPlay = false
+    private lateinit var audioManager: AudioManager
 
     private val mainViewModel: MainViewModel by viewModels {
         MediaViewModelFactory((application as ExpPlayerDemoApplication).repository)
@@ -66,7 +69,7 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
 
         initReceiver()
         initView()
-        mainViewModel.initLogFile()
+
     }
 
     override fun onResume() {
@@ -76,6 +79,22 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
             controller.hide(WindowInsetsCompat.Type.systemBars())
             controller.systemBarsBehavior =
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+        checkIsMusicActive()
+    }
+
+    private fun checkIsMusicActive() {
+        MainScope().launch {
+            while (true) {
+                delay(500L)
+                audioManager = baseContext.getSystemService(AUDIO_SERVICE) as AudioManager
+                Log.i(TAG, "audioManager.isMusicActive:" + audioManager.isMusicActive)
+                if (audioManager.isMusicActive) {
+                    _isMusicActive.setImageResource(R.drawable.ic_play_circle_filled)
+                } else {
+                    _isMusicActive.setImageResource(R.drawable.ic_play_circle_filled_no)
+                }
+            }
         }
     }
 
@@ -107,6 +126,7 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
         _recyclerView = _binding.rvPlaylist
         _playerView = _binding.videoView
         _floatBtn = _binding.floatBtn
+        _isMusicActive = _binding.isMusicActiveIcon
         builderForInfoDialog = CustomDialog.Builder(this)
         mediaListAdapter = MediaListAdapter()
         mediaListAdapter.addItemClickListener(this)
@@ -120,9 +140,9 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
                 )
             )
         }
-        mainViewModel.allMediaInfo.observe(this, { medias ->
+        mainViewModel.allMediaInfo.observe(this) { medias ->
             mediaListAdapter.submitList(medias)
-        })
+        }
 
         _player = ExoPlayer.Builder(this).build().apply {
             playWhenReady = true
@@ -149,6 +169,9 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
             override fun scanBegin() {
                 Log.i(TAG, "Scan Begin: $rootPath")
                 _player.clearMediaItems()
+                mainViewModel.deleteAll()
+                isAutoPlay = false
+                _floatBtn.isEnabled = false
             }
 
             override fun scanComplete(timeConsuming: Long) {
@@ -162,8 +185,9 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
                 mainViewModel.currentPosition = 0
 
                 MainScope().launch {
-                    delay(2000)
+                    delay(3000)
                     playMedia()
+                    _floatBtn.isEnabled = true
                 }
             }
 
@@ -173,19 +197,25 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
         })
     }
 
-    // Player Listener
+    fun packageMediaFile(file: File): MediaInfo {
+        return MediaInfo(
+            file.name,
+            file.extension,
+            file.length().toInt(),
+            file.name,
+            file.length().toInt(),
+            file.canonicalPath
+        )
+    }
+
+    /** PLAYER STATUS **/
     override fun onPlayerError(error: PlaybackException) {
         super.onPlayerError(error)
         Log.i(TAG, "Player ERROR: ${error.errorCodeName},   ${error.message}")
         mainViewModel.currentMediaInfo.isAbility = 2
         mediaListAdapter.notifyItemChanged(mainViewModel.currentPosition)
         mainViewModel.saveLog("播放失败: ${error.errorCodeName},   ${error.message}\n\n")
-        if (isAutoPlay) {
-            MainScope().launch {
-                delay(500)
-                playNextMedia()
-            }
-        }
+        delayPlayNextMedia()
     }
 
     override fun onPlaybackStateChanged(playbackState: Int) {
@@ -222,7 +252,10 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
                 DELAY_TIME = if (_player.duration < DELAY_TIME) _player.duration else DELAY_TIME
                 object : CountDownTimer(DELAY_TIME * 1000L, 100L) {
                     override fun onTick(millisUntilFinished: Long) {
-                        if (mainViewModel.currentMediaInfo.isAbility == 2)
+                        if (mainViewModel.currentMediaInfo.isAbility == 2 ||
+                            !isAutoPlay ||
+                            !isPlaying
+                        )
                             this.cancel()
                     }
 
@@ -230,7 +263,7 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
                         mainViewModel.currentMediaInfo.isAbility = 1
                         mediaListAdapter.notifyItemChanged(mainViewModel.currentPosition)
                         mainViewModel.saveLog("播放成功\n\n")
-                        playNextMedia()
+                        delayPlayNextMedia()
                     }
                 }.start()
             }
@@ -279,20 +312,8 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
         if (_player.isPlaying) {
             _player.stop()
         }
-        mainViewModel.deleteAll()
         initScan()
         _scanFile.startAsyncScan()
-    }
-
-    fun packageMediaFile(file: File): MediaInfo {
-        return MediaInfo(
-            file.name,
-            file.extension,
-            file.length().toInt(),
-            file.name,
-            file.length().toInt(),
-            file.canonicalPath
-        )
     }
 
     override fun onPlayListener(mediaInfo: MediaInfo, position: Int) {
@@ -302,9 +323,13 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
         playMedia()
     }
 
-    private fun playNextMedia() {
+    private fun delayPlayNextMedia() {
+        if (!isAutoPlay) return
         mainViewModel.currentPosition++
-        playMedia()
+        MainScope().launch {
+            delay(500)
+            playMedia()
+        }
     }
 
     private fun playMedia() {
@@ -315,7 +340,7 @@ class MainActivity : AppCompatActivity(), MediaListAdapter.onItemClickListener, 
         } else if (mainViewModel.allMediaInfo.value?.get(mainViewModel.currentPosition) != null) {
             mainViewModel.currentMediaInfo =
                 mainViewModel.allMediaInfo.value!![mainViewModel.currentPosition]
-            _recyclerView.scrollToPosition(mainViewModel.currentPosition)
+            _recyclerView.smoothScrollToPosition(mainViewModel.currentPosition)
 
             if (!mainViewModel.currentMediaInfo.path.isNullOrEmpty()) {
 
